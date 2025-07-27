556e89bb9a01a26292f8a6e56bdb99287d3220c4
kushagra vyas <kushagravyas1122@gmail.com>
2025-07-27 16:12:33 +0530

    Your commit message

---
+++ arrays.js
@@ -0,0 +1,34 @@
+// Find largest number in Array.
+
+function largestNumber (arr) {
+    let max = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) max = arr[i];
+    }
+
+    return max;
+}
+
+console.log(largestNumber([5,3,6,9,2,1]));
+
+
+function findLargest (arr) {
+    let max = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < max) max = arr[i]
+    }
+
+    return max;
+}
+
+console.log(findLargest([2,3,4,5,6,7,8]))
+
+function findNumber (arr) {
+    let min = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < min) min = arr[i];
+    }
+    return min;
+}
+
+console.log(findNumber([1,3,4,5,7,8]))
--- arrys.js
+++
@@ -1,5 +0,0 @@
-// Find largest number in Array.
-
-function largestNumber (arr){
-    
-}
